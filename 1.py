2b3865de7200bded251dcb061cb8b8aada050742
31_buithuphuong17a3 <148468667+31-buithuphuong-17a3hn@users.noreply.github.com>
2023-11-18 13:50:38 +0700

    init project

--- 1.py
+++ 1.py
@@ -3,5 +3,5 @@ number = float(input("Nhập một số: "))
 if number > 0:
     square = number ** 2
     print("Bình phương của", number, "là", square)
-else:
+else: 
     print("Số không hợp lệ. Vui lòng nhập một số dương.")
