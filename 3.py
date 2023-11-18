b64fd1bfb996049712806ddaf85ba213b7da2581
31_buithuphuong17a3 <148468667+31-buithuphuong-17a3hn@users.noreply.github.com>
2023-11-18 13:50:07 +0700

    init project

--- 3.py
+++ 3.py
@@ -4,4 +4,4 @@ n = int(input("Nhập số tự nhiên n: "))
 print("Các số chia hết cho", m, "trong khoảng từ 1 đến", n, "là:")
 for i in range(1, n + 1):
     if i % m == 0:
-        print(i, end=" ")
+        print(i, end=" ") 
